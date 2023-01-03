727d572316dd956e114fc1ea5a741104e4dc1004
amirnezhad <amirnezhadsomayyeh@gmail.com>
2023-01-03 06:24:32 +0330

    created nav expanded for toggler in css and js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
+// NAV TOGGLER
+
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav");
+toggler.addEventListener("click", (e) => {
+  navbar.classList.toggle("nav__expanded");
+});
+
 
